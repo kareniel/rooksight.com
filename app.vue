ba6672ec09e4c304b87179bb13e44df02e7340e8
Jonathan Dupré <419946+kareniel@users.noreply.github.com>
2024-02-15 21:14:22 -0500

    init

---
+++ app.vue
@@ -0,0 +1,35 @@
+<template>  
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+const title = ref('Rooksight');
+
+useHead({
+  title: title,
+  meta: [{
+    name: 'viewport',
+    content: "width=device-width, initial-scale=1.0",
+  }, {
+    name: 'description',
+    content: "To reduce the cybersecurity burden of entrepreneurs.",
+  }, {
+    name: 'keywords',
+    content: "Cybersecurity, startup, SaaS, B2B, compliance, SOC 2, ISO 27001",
+  }, {
+    name: 'author',
+    content: "Rooksight Advisory",
+  }],
+  script: [{
+    src: "https://www.googletagmanager.com/gtag/js?id=GTM-5RG8GNXB",
+    async: true
+  }],
+  link: [{
+    rel: "stylesheet",
+    href: "/css/style.css"
+  }]
+});
+
+</script>
